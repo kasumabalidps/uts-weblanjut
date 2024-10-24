0740bfdf86d3997df45c384113a813733c84a03b
Kasuma <113836758+kasumabalidps@users.noreply.github.com>
2024-10-24 18:44:46 +0800

    Update header.php

--- app/views/template/header.php
+++ app/views/template/header.php
@@ -17,7 +17,6 @@
                         <div class="ml-6 flex items-baseline space-x-4">
                             <a href="<?= BASEURL; ?>" class="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-semibold">Beranda</a>
                             <a href="<?= BASEURL; ?>/pengisian" class="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-semibold">Pendaftaran</a>
-                            <a href="#" class="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-semibold">Tentang</a>
                             <a href="#" class="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-semibold">Kontak</a>
                         </div>
                     </div>
