1a5246de1fcd404cc31ac310a8d73a3bd30e380e
Kasuma <113836758+kasumabalidps@users.noreply.github.com>
2024-10-26 14:20:51 +0800

    Update footer.php

--- app/views/template/footer.php
+++ app/views/template/footer.php
@@ -1,2 +1,5 @@
+    <footer class="bg-gray-800 text-white py-5 text-center">
+        <p class="text-sm">&copy; 2024 <span class="font-bold">Nando</span>. Hak Cipta Dilindungi.</p>
+    </footer>
 </body>
 </html>
